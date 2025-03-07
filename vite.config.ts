88eb517d6eaaf0755a22179c83e105fd512ba0f5
lakhilohia <advlakhilohia@gmail.com>
2025-03-08 00:07:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/studymadease8761/", // Yeh aapke repo ke naam ka hona chahiye
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
